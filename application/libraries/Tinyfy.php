6efbee7e5f977635e56edec0a4c4daf680898505
sembolbu <sembolbu@yandex.com>
2020-07-07 08:54:25 +0300

    Update Tinyfy.php

--- application/libraries/Tinyfy.php
+++ application/libraries/Tinyfy.php
@@ -3,8 +3,9 @@
 class Tinyfy
 {
 
-	public function compress($image)
+	public function compress($image) //Methodumuz sıkıştırılacak resmin tam dizinini parametre olarak alır.
 	{
+		//Gerekli dosyaların import edilmesi.
 		$CI =& get_instance();
 		require_once("lib/Tinify/Exception.php");
 		require_once("lib/Tinify/ResultMeta.php");
@@ -13,16 +14,17 @@ class Tinyfy
 		require_once("lib/Tinify/Client.php");
 		require_once("lib/Tinify.php");
 
+		//Api ile bağlantı denenir.
 		try {
 
-			\Tinify\setKey($CI->config->item("tinyfy_api"));
-			$validate = \Tinify\validate();
+			\Tinify\setKey($CI->config->item("tinyfy_api")); //config.php ye eklenen api anahtarı burada kullanılır.
+			$validate = \Tinify\validate(); //Bağlantı sonucu değişkene atanır.
 			if ($validate == TRUE) {
-				\Tinify\fromFile($image)->toFile($image);
+				\Tinify\fromFile($image)->toFile($image); //Api ile bağlantı başarılı ise işlem yapılır. resim önceki ile değiştirilir. isterseniz toFile içerisindeki kısmı değiştirerek resmi farklı bir konuma farklı bir isimde kaydedebilirsiniz.
 			}
 
 		} catch (\Tinify\Exception $e) {
-			echo "Tinify API HATALI";
+			echo "Tinify API HATALI"; //Api bağlantısı başarısız ise ekrana hata basar.
 		}
 	}
 
